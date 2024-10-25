459f358a11569fda9ca7d7c9345068161f6d85a4
zzoo0123 <0123suh@gmail.com>
2024-10-26 00:00:46 +0900

    feat: config 추가

---
+++ user/src/main/java/com/carrot/user/config/ClientConfig.java
@@ -0,0 +1,20 @@
+package com.carrot.user.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ClientConfig {
+
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
