7515ca926c9cded310a91ec4ea912659c83ec1c9
zzoo0123 <0123suh@gmail.com>
2024-10-29 01:56:30 +0900

    feat: config

---
+++ item/src/main/java/com/carrot/item/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.carrot.item.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.*;
+import io.swagger.v3.oas.models.security.*;
+import org.springframework.context.annotation.*;
+
+import java.util.Arrays;
+
+@OpenAPIDefinition(
+        info = @Info(title = "얼렁뚱땅근 API 명세서",
+                description = "API 명세서",
+                version = "v1")
+)
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Arrays.asList(securityRequirement));
+    }
+}
+
